0b46317c4ac8aab0aa1bd76f0d9481b47a24b357
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-05-10 19:06:41 +0530

    chore: fix string and para examples and helpers

--- addon/helpers/chance-paragraph.js
+++ addon/helpers/chance-paragraph.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 import Chance from 'chance';
 
-export default helper(function chanceParagraph(params/*, hash */ ) {
+export default helper(function chanceParagraph(params, { sentences } ) {
   const chance = new Chance();
-  return chance.paragraph();
+  return chance.paragraph({sentences});
 });
--- addon/helpers/chance-string.js
+++ addon/helpers/chance-string.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 import Chance from 'chance';
 
-export default helper(function chanceString(params/*, hash */ ) {
+export default helper(function chanceString(params, { length, pool, alpha, casing, symbols } ) {
   const chance = new Chance();
-  return chance.string();
+  return chance.string({ length, pool, alpha, casing, symbols});
 });
