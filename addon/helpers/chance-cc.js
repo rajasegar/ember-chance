1b316842dbe946265a558893ab4132258994ec48
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-05-14 11:17:37 +0530

    fix: update chance-cc helper to accept types

--- addon/helpers/chance-cc.js
+++ addon/helpers/chance-cc.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 import Chance from 'chance';
 
-export default helper(function chanceCc(params/*, hash */ ) {
+export default helper(function chanceCc(params,{type} ) {
   const chance = new Chance();
-  return chance.cc();
+  return chance.cc({type});
 });
