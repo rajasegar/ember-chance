736939e52d0bd111cf3bb21fd8d23955c84264c8
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-05-14 10:50:19 +0530

    fix: update color helper to use options

--- addon/helpers/chance-color.js
+++ addon/helpers/chance-color.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 import Chance from 'chance';
 
-export default helper(function chanceColor(params/*, hash */ ) {
+export default helper(function chanceColor(params, { format, grayscale, casing } ) {
   const chance = new Chance();
-  return chance.color();
+  return chance.color({format, grayscale, casing});
 });
