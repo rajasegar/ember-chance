bf02339a175e0b073a71419fdcdc3d9fef4c3f7f
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-05-14 11:39:14 +0530

    fix: update cc-type helper to accept raw option
    1. Removing commitlint

--- addon/helpers/chance-cc-type.js
+++ addon/helpers/chance-cc-type.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 import Chance from 'chance';
 
-export default helper(function chanceCcType(params/*, hash */ ) {
+export default helper(function chanceCcType(params, {raw} ) {
   const chance = new Chance();
-  return chance.cc_type();
+  return chance.cc_type({raw});
 });
